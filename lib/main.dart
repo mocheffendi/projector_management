26c2e832bc8a3e0e784deb63f75c9a296be4bdf2
Mochammad Effendi <moch.effendi@gmail.com>
2024-11-15 16:47:50 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'dart:io';
 
-void main() => runApp(const ProjectorApp());
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:convert'; // To handle image encoding as base64
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyBSor1UaXN1VY9g1GeCa3bXVmMI7fFOzE0',
+    appId: '1:898353156158:web:c4b924a8c5abaab5d3416f',
+    messagingSenderId: '898353156158',
+    projectId: 'myflutter-49607',
+    storageBucket: 'myflutter-49607.firebasestorage.app',
+  ));
+
+  await Firebase.initializeApp();
+  runApp(const ProjectorApp());
+}
 
 class ProjectorApp extends StatelessWidget {
   const ProjectorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'Roboto', // Set the default font family
-      ),
-      home: const ProjectorHomePage(),
+    return const MaterialApp(
+      home: ProjectorHomePage(),
     );
   }
 }
@@ -25,53 +43,6 @@ class ProjectorHomePage extends StatefulWidget {
 }
 
 class _ProjectorHomePageState extends State<ProjectorHomePage> {
-  List<Map<String, dynamic>> projectors = [
-    {
-      'model': 'EPSON EB-X51',
-      'sn': 'X8A43202321',
-      'status': 'not use',
-      'image': 'assets/epson_x51.png'
-    },
-    {
-      'model': 'EPSON EB-X51',
-      'sn': 'X8A43201960',
-      'status': 'not use',
-      'image': 'assets/epson_x51.png'
-    },
-    {
-      'model': 'EPSON EB-1776w',
-      'sn': 'RESK6900139',
-      'status': 'not use',
-      'image': 'assets/epson_1776w.png'
-    },
-    {
-      'model': 'EPSON EB-1776w',
-      'sn': 'RESK6900168',
-      'status': 'not use',
-      'image': 'assets/epson_1776w.png'
-    },
-    {
-      'model': 'EPSON EB-2155w',
-      'sn': 'X3M5740048L',
-      'status': 'not use',
-      'image': 'assets/epson_2155w.png'
-    },
-    {
-      'model': 'PANASONIC PT-EX800',
-      'sn': 'DD9240004',
-      'status': 'Ballroom A',
-      'image': 'assets/panasonic_ex800.png',
-      'occupied': true
-    },
-    {
-      'model': 'SONY VPL-CH370',
-      'sn': '7008744 848s',
-      'status': 'Grand Ballroom',
-      'image': 'assets/sony_ch370.png',
-      'occupied': true
-    },
-  ];
-
   final List<String> roomOptions = [
     'not use',
     'Grand Ballroom',
@@ -100,35 +71,237 @@ class _ProjectorHomePageState extends State<ProjectorHomePage> {
     'The Heritage'
   ];
 
-  @override
-  void initState() {
-    super.initState();
-    _loadStatuses();
+  Future<List<Map<String, dynamic>>> fetchProjectors() async {
+    try {
+      QuerySnapshot snapshot =
+          await FirebaseFirestore.instance.collection('projectors').get();
+      List<Map<String, dynamic>> projectors = snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return {
+          'id': doc.id,
+          'model': data['model'],
+          'sn': data['sn'],
+          'status': data['status'],
+          'image': data['image'],
+          'lastUpdated':
+              data['lastUpdated']?.toDate(), // Handle the date conversion
+          'occupied': data['status'] != 'not use',
+        };
+      }).toList();
+      return projectors;
+    } catch (e) {
+      print("Error fetching projectors: $e");
+      return [];
+    }
   }
 
-  Future<void> _loadStatuses() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      for (int i = 0; i < projectors.length; i++) {
-        String savedStatus =
-            prefs.getString('projector_status_$i') ?? 'not use';
-        projectors[i]['status'] = savedStatus;
-        projectors[i]['occupied'] = savedStatus != 'not use';
-      }
-    });
+  Future<void> updateStatus(String id, String newStatus) async {
+    try {
+      await FirebaseFirestore.instance.collection('projectors').doc(id).update({
+        'status': newStatus,
+        'lastUpdated':
+            FieldValue.serverTimestamp(), // Update timestamp on status change
+      });
+      setState(() {});
+    } catch (e) {
+      print("Error updating status: $e");
+    }
   }
 
-  Future<void> _saveStatus(int index, String status) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('projector_status_$index', status);
+  Future<void> updateProjector(String id, String model, String sn,
+      String base64Image, String status) async {
+    try {
+      await FirebaseFirestore.instance.collection('projectors').doc(id).update({
+        'model': model,
+        'sn': sn,
+        'status': status,
+        'image': base64Image, // Save base64 image string
+        'lastUpdated': FieldValue.serverTimestamp(), // Update timestamp on edit
+      });
+      setState(() {});
+    } catch (e) {
+      print("Error updating projector: $e");
+    }
   }
 
-  void updateStatus(int index, String newStatus) {
-    setState(() {
-      projectors[index]['status'] = newStatus;
-      projectors[index]['occupied'] = newStatus != 'not use';
-    });
-    _saveStatus(index, newStatus);
+  Future<void> deleteProjector(String id) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('projectors')
+          .doc(id)
+          .delete();
+      setState(() {});
+    } catch (e) {
+      print("Error deleting projector: $e");
+    }
+  }
+
+  Future<void> addProjector(String model, String sn, String base64Image) async {
+    try {
+      await FirebaseFirestore.instance.collection('projectors').add({
+        'model': model,
+        'sn': sn,
+        'status': 'not use',
+        'image': base64Image, // Save base64 image string
+        'lastUpdated':
+            FieldValue.serverTimestamp(), // Add timestamp when adding
+      });
+      setState(() {});
+    } catch (e) {
+      print("Error adding projector: $e");
+    }
+  }
+
+  Future<void> _showAddProjectorDialog() async {
+    final TextEditingController modelController = TextEditingController();
+    final TextEditingController snController = TextEditingController();
+    String? base64Image;
+    // String status = 'not use';
+
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add New Projector'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: modelController,
+                decoration: const InputDecoration(labelText: 'Model'),
+              ),
+              TextField(
+                controller: snController,
+                decoration: const InputDecoration(labelText: 'Serial Number'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final result = await FilePicker.platform.pickFiles(
+                    type: FileType.image,
+                  );
+                  if (result != null) {
+                    final bytes = result.files.single.bytes;
+                    base64Image = base64Encode(bytes!);
+                  }
+                },
+                child: const Text('Pick Image'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (modelController.text.isNotEmpty &&
+                    snController.text.isNotEmpty &&
+                    base64Image != null) {
+                  addProjector(
+                      modelController.text, snController.text, base64Image!);
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _showEditProjectorDialog(Map<String, dynamic> projector) async {
+    final TextEditingController modelController =
+        TextEditingController(text: projector['model']);
+    final TextEditingController snController =
+        TextEditingController(text: projector['sn']);
+    String? base64Image = projector['image'];
+    String status = projector['status'];
+
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Edit Projector'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: modelController,
+                decoration: const InputDecoration(labelText: 'Model'),
+              ),
+              TextField(
+                controller: snController,
+                decoration: const InputDecoration(labelText: 'Serial Number'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final result = await FilePicker.platform.pickFiles(
+                    type: FileType.image,
+                  );
+                  if (result != null) {
+                    final bytes = result.files.single.bytes;
+                    base64Image = base64Encode(bytes!);
+                  }
+                },
+                child: const Text('Pick Image'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (modelController.text.isNotEmpty &&
+                    snController.text.isNotEmpty &&
+                    base64Image != null) {
+                  updateProjector(projector['id'], modelController.text,
+                      snController.text, base64Image!, status);
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Update'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _showDeleteConfirmationDialog(String id) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Delete Projector'),
+          content:
+              const Text('Are you sure you want to delete this projector?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                deleteProjector(id);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -140,74 +313,117 @@ class _ProjectorHomePageState extends State<ProjectorHomePage> {
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8.0),
-        itemCount: projectors.length,
-        itemBuilder: (context, index) {
-          final projector = projectors[index];
-          Color cardColor = projector['status'] == 'not use'
-              ? Colors.green.shade100
-              : Colors.white;
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchProjectors(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No projectors found.'));
+          } else {
+            final projectors = snapshot.data!;
+            return ListView.builder(
+              itemCount: projectors.length,
+              itemBuilder: (context, index) {
+                final projector = projectors[index];
+                Color cardColor = projector['status'] == 'not use'
+                    ? Colors.green.shade100
+                    : Colors.white;
 
-          return Card(
-            color: cardColor,
-            margin: const EdgeInsets.symmetric(vertical: 6.0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  // Projector Image
-                  Image.asset(
-                    projector['image'],
-                    width: 150,
-                    height: 60,
-                    fit: BoxFit.cover,
-                  ),
-                  const SizedBox(width: 10),
-                  // Projector Details
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                return Card(
+                  color: cardColor,
+                  margin: const EdgeInsets.symmetric(vertical: 6.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
                       children: [
-                        Text(
-                          '${projector['model']}',
-                          style: const TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
+                        // Projector Image
+                        Image.memory(
+                          base64Decode(projector['image'] ?? ''),
+                          width: 160,
+                          height: 60,
+                          fit: BoxFit.cover,
                         ),
-                        Text('SN: ${projector['sn']}'),
-                        if (projector['occupied'] == true)
-                          Text(
-                            'Occupied @${projector['status']}, ${DateTime.now()}',
-                            style: const TextStyle(color: Colors.red),
-                          )
-                        else
-                          const Text(
-                            'not Occupied / @AV_Warehouse',
-                            style: TextStyle(color: Colors.green),
+                        const SizedBox(width: 10),
+                        // Projector Details
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '${projector['model']}',
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Text('SN: ${projector['sn']}'),
+                              if (projector['occupied'])
+                                Text('Occupied @${projector['status']}',
+                                    style: const TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ))
+                              else
+                                const Text(
+                                  'Not Occupied / @AV_Warehouse',
+                                  style: TextStyle(color: Colors.green),
+                                ),
+                              Text(
+                                'Last Updated: ${projector['lastUpdated'] != null ? projector['lastUpdated']!.toLocal().toString() : 'N/A'}',
+                                style: const TextStyle(
+                                    fontSize: 12, color: Colors.grey),
+                              ),
+
+                              // Dropdown for status change
+                              DropdownButton<String>(
+                                value: projector['status'],
+                                onChanged: (newValue) {
+                                  if (newValue != null) {
+                                    updateStatus(projector['id'], newValue);
+                                  }
+                                },
+                                items: roomOptions.map((room) {
+                                  return DropdownMenuItem<String>(
+                                    value: room,
+                                    child: Text(room),
+                                  );
+                                }).toList(),
+                              ),
+                            ],
                           ),
-                        DropdownButton<String>(
-                          value: projector['status'],
-                          onChanged: (newValue) {
-                            if (newValue != null) {
-                              updateStatus(index, newValue);
-                            }
-                          },
-                          items: roomOptions.map((room) {
-                            return DropdownMenuItem<String>(
-                              value: room,
-                              child: Text(room),
-                            );
-                          }).toList(),
+                        ),
+                        // Icons for Edit and Delete
+                        Column(
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.edit, color: Colors.blue),
+                              onPressed: () {
+                                _showEditProjectorDialog(projector);
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.red),
+                              onPressed: () {
+                                _showDeleteConfirmationDialog(projector['id']);
+                              },
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ),
-                  // Dropdown Menu for Room Selection
-                ],
-              ),
-            ),
-          );
+                );
+              },
+            );
+          }
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddProjectorDialog,
+        // ignore: prefer_const_constructors
+        child: Icon(Icons.add),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:projector_management/main.dart';
+import 'package:projector_management/main_shared_pref.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
