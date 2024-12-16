5a3bcaa0b2a28cad089ffbe9069d185f8e1c434f
Mochammad Effendi <moch.effendi@gmail.com>
2024-12-16 13:19:04 +0700

    update

--- buildweb/flutter_bootstrap.js
+++ buildweb/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3398272491"
+    serviceWorkerVersion: "4257688657"
   }
 });
