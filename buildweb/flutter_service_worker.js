10dc0c342417e7ed645812e69ee9476a94939fc2
Mochammad Effendi <moch.effendi@gmail.com>
2024-12-19 16:09:30 +0700

    update

--- buildweb/flutter_service_worker.js
+++ buildweb/flutter_service_worker.js
@@ -37,14 +37,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c82b8612524dfa18da074c940dd2779f
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "b94c1e827207031e55a6fd3cc4237aa5",
+"flutter_bootstrap.js": "6ed2babf78b1034695db0285340e98bc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "eb792e2f7bf404abdb165eddac5d04bc",
 "/": "eb792e2f7bf404abdb165eddac5d04bc",
-"main.dart.js": "e31b98f50733bcde6882cc3b262bf2a0",
+"main.dart.js": "f7326cc99bcf4b857de49ebef4a2523f",
 "manifest.json": "4fe2bdc102df2d7a9af04cd82d6ecb23",
 "version.json": "73855b5d94312a332a3e56d444d7168c"};
 // The application shell files that are downloaded before a service worker can
--- lib/utility/generatepdfuniversal.dart
+++ lib/utility/generatepdfuniversal.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
-// import 'dart:developer';
-// import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:pdf/pdf.dart';
@@ -144,13 +141,19 @@ pw.Widget _builddeviceCardpw(Map<String, dynamic> device, Uint8List info,
 
   PdfColor cardColor = PdfColors.grey100;
 
-  final imageInfo = pw.MemoryImage(info); // Convert to MemoryImage
-  final imageCondition = pw.MemoryImage(condition);
-  final imageTime = pw.MemoryImage(time);
-  final imageStatus = pw.MemoryImage(status);
-  final imageCalendar = pw.MemoryImage(calendar);
+  final String deviceModel = device['model'] ?? 'Unknown Model';
+  final String deviceSN = device['sn'] ?? '';
+  final String deviceCondition = device['condition'] ?? 'Unknown Condition';
+  final String deviceStatus = device['status'] ?? 'Unknown Status';
+  final String deviceRemarks = device['remark'] ?? '';
+
+  // final imageInfo = pw.MemoryImage(info); // Convert to MemoryImage
+  // final imageCondition = pw.MemoryImage(condition);
+  // final imageTime = pw.MemoryImage(time);
+  // final imageStatus = pw.MemoryImage(status);
+  // final imageCalendar = pw.MemoryImage(calendar);
   // Example device status
-  final String deviceStatus = device['status'];
+  // final String deviceStatus = device['status'];
   final String statusLabel;
   final PdfColor statusColor;
 
@@ -167,113 +170,243 @@ pw.Widget _builddeviceCardpw(Map<String, dynamic> device, Uint8List info,
     statusColor = PdfColors.red;
   }
 
+  // return pw.Container(
+  //   decoration: pw.BoxDecoration(
+  //       color: cardColor, borderRadius: pw.BorderRadius.circular(8)),
+  //   margin: const pw.EdgeInsets.symmetric(vertical: 5.0),
+  //   padding: const pw.EdgeInsets.all(5.0),
+  //   child: pw.Row(
+  //     crossAxisAlignment: pw.CrossAxisAlignment.start,
+  //     children: [
+  //       device['image'] != null
+  //           ? pw.Image(
+  //               pw.MemoryImage(base64Decode(device['image'] ?? '')),
+  //               width: 150,
+  //               height: 100,
+  //               fit: pw.BoxFit.fitWidth,
+  //             )
+  //           : pw.Container(
+  //               width: 150,
+  //               height: 100,
+  //               decoration: const pw.BoxDecoration(color: PdfColors.grey300),
+  //               child: pw.Center(child: pw.Text("No Image")),
+  //             ),
+  //       pw.SizedBox(width: 5),
+  //       pw.Expanded(
+  //         child: pw.Column(
+  //           crossAxisAlignment: pw.CrossAxisAlignment.start,
+  //           children: [
+  //             pw.Text(
+  //               '${device['model']}',
+  //               style: pw.TextStyle(
+  //                 fontSize: 16,
+  //                 fontWeight: pw.FontWeight.bold,
+  //               ),
+  //             ),
+  //             pw.Row(children: [
+  //               pw.Image(
+  //                 imageInfo,
+  //                 width: 12,
+  //                 height: 12,
+  //                 fit: pw.BoxFit.contain,
+  //               ),
+  //               pw.SizedBox(width: 8),
+  //               pw.Text('SN: ${device['sn']}'),
+  //             ]),
+  //             pw.Row(children: [
+  //               pw.Image(
+  //                 imageCondition,
+  //                 width: 12,
+  //                 height: 12,
+  //                 fit: pw.BoxFit.contain,
+  //               ),
+  //               pw.SizedBox(width: 8),
+  //               pw.Text('Condition: ${device['condition']}'),
+  //             ]),
+  //             pw.Row(children: [
+  //               pw.Image(
+  //                 imageTime,
+  //                 width: 12,
+  //                 height: 12,
+  //                 fit: pw.BoxFit.contain,
+  //               ),
+  //               pw.SizedBox(width: 9),
+  //               pw.Text('${device['remark']}'),
+  //             ]),
+  //             pw.Row(children: [
+  //               pw.Image(
+  //                 imageStatus,
+  //                 width: 12,
+  //                 height: 12,
+  //                 fit: pw.BoxFit.contain,
+  //               ),
+  //               pw.SizedBox(width: 8),
+  //               pw.Text(
+  //                 statusLabel,
+  //                 style: pw.TextStyle(
+  //                   color: statusColor,
+  //                 ),
+  //               ),
+  //             ]),
+  //             pw.Row(children: [
+  //               // pw.Image(iconImage, width: 100, height: 100),
+  //               pw.Image(
+  //                 imageCalendar,
+  //                 width: 12,
+  //                 height: 12,
+  //                 fit: pw.BoxFit.contain,
+  //               ),
+  //               pw.SizedBox(width: 9),
+  //               pw.Text(
+  //                 'Last Updated: $formattedDate',
+  //                 style: pw.TextStyle(
+  //                     fontSize: 12,
+  //                     color: PdfColors.grey,
+  //                     fontWeight: pw.FontWeight.bold),
+  //               ),
+  //             ]),
+  //             pw.Row(children: [
+  //               pw.Image(
+  //                 imageStatus,
+  //                 width: 12,
+  //                 height: 12,
+  //                 fit: pw.BoxFit.contain,
+  //               ),
+  //               pw.SizedBox(width: 8),
+  //               pw.Text('Status: ${device['status']}'),
+  //             ])
+  //           ],
+  //         ),
+  //       ),
+  //     ],
+  //   ),
+  // );
+
   return pw.Container(
+    margin: const pw.EdgeInsets.symmetric(vertical: 2, horizontal: 4),
+    padding: const pw.EdgeInsets.all(16),
     decoration: pw.BoxDecoration(
-        color: cardColor, borderRadius: pw.BorderRadius.circular(8)),
-    margin: const pw.EdgeInsets.symmetric(vertical: 5.0),
-    padding: const pw.EdgeInsets.all(5.0),
-    child: pw.Row(
+      borderRadius: pw.BorderRadius.circular(20),
+      gradient: pw.LinearGradient(
+        colors: [
+          PdfColors.white,
+          statusColor,
+        ],
+        begin: pw.Alignment.topLeft,
+        end: pw.Alignment.bottomRight,
+      ),
+      // boxShadow: [
+      //   BoxShadow(
+      //     color: Theme.of(context).colorScheme.tertiaryFixed,
+      //     blurRadius: 10,
+      //     offset: const Offset(5, 5),
+      //   ),
+      //   BoxShadow(
+      //     color: Theme.of(context).colorScheme.onTertiaryFixed,
+      //     blurRadius: 10,
+      //     offset: const Offset(-5, -5),
+      //   ),
+      // ],
+    ),
+    child: pw.Column(
       crossAxisAlignment: pw.CrossAxisAlignment.start,
       children: [
-        device['image'] != null
-            ? pw.Image(
-                pw.MemoryImage(base64Decode(device['image'] ?? '')),
-                width: 150,
-                height: 100,
-                fit: pw.BoxFit.fitWidth,
-              )
-            : pw.Container(
-                width: 150,
-                height: 100,
-                decoration: const pw.BoxDecoration(color: PdfColors.grey300),
-                child: pw.Center(child: pw.Text("No Image")),
+        // Header
+        pw.Row(
+          mainAxisAlignment: pw.MainAxisAlignment.spaceAround,
+          children: [
+            pw.Expanded(
+              child: pw.Text(deviceModel,
+                  style: pw.TextStyle(
+                      fontSize: 16, fontWeight: pw.FontWeight.bold)),
+            ),
+            pw.Container(
+              padding: const pw.EdgeInsets.all(4),
+              decoration: pw.BoxDecoration(
+                color: statusColor,
+                borderRadius: pw.BorderRadius.circular(8),
               ),
-        pw.SizedBox(width: 5),
-        pw.Expanded(
-          child: pw.Column(
-            crossAxisAlignment: pw.CrossAxisAlignment.start,
-            children: [
-              pw.Text(
-                '${device['model']}',
+              child: pw.Text(
+                deviceStatus,
                 style: pw.TextStyle(
-                  fontSize: 16,
+                  color: PdfColors.white,
+                  fontSize: 12,
                   fontWeight: pw.FontWeight.bold,
                 ),
               ),
-              pw.Row(children: [
-                pw.Image(
-                  imageInfo,
-                  width: 12,
-                  height: 12,
-                  fit: pw.BoxFit.contain,
-                ),
-                pw.SizedBox(width: 8),
-                pw.Text('SN: ${device['sn']}'),
-              ]),
-              pw.Row(children: [
-                pw.Image(
-                  imageCondition,
-                  width: 12,
-                  height: 12,
-                  fit: pw.BoxFit.contain,
-                ),
-                pw.SizedBox(width: 8),
-                pw.Text('Condition: ${device['condition']}'),
-              ]),
-              pw.Row(children: [
-                pw.Image(
-                  imageTime,
-                  width: 12,
-                  height: 12,
-                  fit: pw.BoxFit.contain,
-                ),
-                pw.SizedBox(width: 9),
-                pw.Text('${device['remark']}'),
-              ]),
-              pw.Row(children: [
-                pw.Image(
-                  imageStatus,
-                  width: 12,
-                  height: 12,
-                  fit: pw.BoxFit.contain,
-                ),
-                pw.SizedBox(width: 8),
-                pw.Text(
-                  statusLabel,
-                  style: pw.TextStyle(
-                    color: statusColor,
+            ),
+          ],
+        ),
+        pw.SizedBox(height: 8),
+        // Image & Details
+        pw.Row(
+          children: [
+            // Device Image
+            device['image'] != null && device['image']!.isNotEmpty
+                ? pw.Image(
+                    pw.MemoryImage(base64Decode(device['image'])),
+                    width: 120,
+                    height: 80,
+                    fit: pw.BoxFit.fitWidth,
+                  )
+                : pw.Container(
+                    width: 120,
+                    height: 80,
+                    decoration:
+                        const pw.BoxDecoration(color: PdfColors.grey300),
+                    child: pw.Center(child: pw.Text("No Image")),
                   ),
+            pw.SizedBox(width: 16),
+            // Device Info
+            pw.Expanded(
+              child: pw.Column(
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: [
+                  pw.Text(
+                    'SN: $deviceSN',
+                    style: const pw.TextStyle(
+                        fontSize: 12, color: PdfColors.black),
+                  ),
+                  pw.SizedBox(height: 4),
+                  pw.Text(
+                    'Condition: $deviceCondition',
+                    style: const pw.TextStyle(
+                        fontSize: 12, color: PdfColors.black),
+                  ),
+                  pw.SizedBox(height: 4),
+                  pw.Text(
+                    'Remarks: $deviceRemarks',
+                    style: const pw.TextStyle(
+                        fontSize: 12, color: PdfColors.black),
+                    maxLines: 2,
+                    // overflow: pw.TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        pw.SizedBox(height: 16),
+        // Footer
+        pw.Row(
+          mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+          children: [
+            pw.Row(
+              children: [
+                pw.Icon(
+                  const pw.IconData(0xe935), // Use appropriate icon data
+                  size: 16,
+                  color: PdfColors.grey600,
                 ),
-              ]),
-              pw.Row(children: [
-                // pw.Image(iconImage, width: 100, height: 100),
-                pw.Image(
-                  imageCalendar,
-                  width: 12,
-                  height: 12,
-                  fit: pw.BoxFit.contain,
-                ),
-                pw.SizedBox(width: 9),
+                pw.SizedBox(width: 4),
                 pw.Text(
-                  'Last Updated: $formattedDate',
-                  style: pw.TextStyle(
-                      fontSize: 12,
-                      color: PdfColors.grey,
-                      fontWeight: pw.FontWeight.bold),
+                  formattedDate,
+                  style:
+                      const pw.TextStyle(fontSize: 12, color: PdfColors.black),
                 ),
-              ]),
-              pw.Row(children: [
-                pw.Image(
-                  imageStatus,
-                  width: 12,
-                  height: 12,
-                  fit: pw.BoxFit.contain,
-                ),
-                pw.SizedBox(width: 8),
-                pw.Text('Status: ${device['status']}'),
-              ])
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ],
     ),
